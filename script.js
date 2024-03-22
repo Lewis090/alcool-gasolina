ab878a28458cbc0bf141e3ee2ddf78bdb29d31e7
Lewis090 <162984256+Lewis090@users.noreply.github.com>
2024-03-22 11:56:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function calcular (){
+function calcular() {
     alcool = document.getElementById("alcool").value
     gasolina = document.getElementById("gasolina").value
     resultado = alcool / gasolina
 
-    if (resultado >0.7) {
+    if (resultado > 0.7) {
         document.getElementById("resultado").innerText = `GASOLINA`
-        document.getElementById("resultado").style.color ='orange'
+        document.getElementById("resultado").style.color = 'orange'
+        document.getElementById("resultado").style.background = "black"
     }
-    if(resultado <0.7){
+    if (resultado < 0.7) {
         document.getElementById("resultado").innerText = `ALCOOL`
-        document.getElementById("resultado").style.color='green'
+        document.getElementById("resultado").style.color = 'green'
+        document.getElementById("resultado").style.background = "black"
     }
 }
